8592920ebda3dfc66c7bc29cf7a1a0412faeaa29
jeanpcoelho <jeanpc0018@gmail.com>
2024-09-20 14:44:58 -0300

    Update NucleotideosTest.java

--- src/test/NucleotideosTest.java
+++ src/test/NucleotideosTest.java
@@ -20,3 +20,26 @@ public class NucleotideosTest {
         for (int i = 0; i < 6; i++) {
             File arquivo = new File(nome + (i+1) + ".txt");
             arquivo.createNewFile();
+
+        }
+        try (BufferedWriter bw1 = new BufferedWriter(new FileWriter("arquivo1.txt"))) {
+            bw1.write("AACTGTCGBA");
+            bw1.newLine();
+        }
+        try (BufferedWriter bw2 = new BufferedWriter(new FileWriter("arquivo2.txt"))) {
+            bw2.write("AAAGTCTGAC");
+            bw2.newLine();
+        }
+        try (BufferedWriter bw3 = new BufferedWriter(new FileWriter("arquivo3.txt"))) {
+            bw3.write("ACGYCAAYOE");
+            bw3.newLine();
+        }
+        try (BufferedWriter bw4 = new BufferedWriter(new FileWriter("arquivo4.txt"))) {
+            bw4.write("BDEFHIJKLM");
+            bw4.newLine();
+        }
+        try (BufferedWriter bw5 = new BufferedWriter(new FileWriter("arquivo5.txt"))) {
+            bw5.write("");
+            bw5.newLine();
+        }
+    }    
